cebef4ec5a14d97800ea1b25ca60d2a6ebc274d0
Aleksei <iamviga@yandex.ru>
2022-04-03 20:45:18 +0300

    Final Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,22 +17,43 @@ public class Main {
         String str1 = sc1.nextLine();
         String[] str1Array = str1.split(" ");
 
-        Comparator<String> compare = new Compare();
-        Arrays.sort(str1Array, compare);
 
+//        самое длинное слово через цикл
+
+        String maxString = str1Array[0];
+        for (String word : str1Array) {
+            System.out.println(maxString);
+            if (maxString.length() <= word.length()) {
+                maxString = word;
+            }
+        }
+        System.out.println("Максимальное слово - " + maxString);
+
+        //        самое короткое слово через цикл
+
+        String minString = str1Array[0];
+        for (int i1 = 0; i1 < str1Array.length; i1++) {
+            System.out.println(minString);
+            if (minString.length() >= str1Array[i1].length()) {
+                minString = str1Array[i1];
+            }
+        }
+        System.out.println("Минимальное слово - " + minString);
+
+//        самое длинное и короткое слово через компаратор
+
+        Comparator<String> stringComparator = new Compare();
+        Arrays.sort(str1Array, stringComparator);
         System.out.println(str1Array[0]);
         System.out.println(str1Array[str1Array.length-1]);
+
 
         for (int letters = 0; letters < 10; letters++) {
             for (String words : str1Array) {
                 if (words.length() <= letters) {
-                    System.out.println(words); return;
+                    System.out.println(words); break;
                 }
             }
         }
-
-
-
-
     }
 }
